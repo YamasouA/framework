84ea333acfa7c54478f3d8850679890be10ec893
YamasouA <akiakiskyhand@gmail.com>
2021-09-21 00:02:10 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # framework
+
+## 参考
+https://c-bata.link/webframework-in-python/wsgi.html
+
+## 
+gunicorn -w 1 hello:application
---
+++ hello.py
@@ -0,0 +1,5 @@
+def application(env, start_response):
+    # start_response(status, headers)
+    start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
+    return [b'Hello World']
+
