8c625ce5e848468a6a0a00838cb8b00d3c39c271
YamasouA <akiakiskyhand@gmail.com>
2021-09-21 00:06:55 +0900

    簡単なルーティング

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
 def application(env, start_response):
     # start_response(status, headers)
-    start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
-    return [b'Hello World']
+    path = env['PATH_INFO']
+    if path == '/':
+        start_response('200 OK', [('Content-type', 'text/plain')])
+        return [b'Hello World']
+    elif path == '/foo':
+        start_response('200 OK', [('Content-type', 'text/plain')])
+        return [b'foo']
+    else:
+        start_response('200 OK', [('Content-type', 'text/plain')])
+        return [b'404 Not Found']
 
